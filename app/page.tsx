a6a244dd572e9d005f85523ffe6423ee6e5468a3
Sergey <m1piks1306@mail.ru>
2024-07-31 18:04:35 +0300

    add component topbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import { Categories, Container, Title } from "@/components/shared";
+import {
+  Container,
+  Title,
+  TopBar,
+} from "@/components/shared";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
-
-        <Categories/>
       </Container>
+      
+      <TopBar />
     </>
   );
 }
--- components/shared/sort-popup.tsx
+++ components/shared/sort-popup.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/lib/utils";
+import { Arrow } from "@radix-ui/react-popover";
+import { ArrowUpDown } from "lucide-react";
 import React from "react";
 
 interface Props {
@@ -7,8 +9,11 @@ interface Props {
 
 export const SortPopup: React.FC<Props> = ({ className }) => {
   return (
-    <div className={cn('', className)}>
-      
+    <div className={cn('inline-flex items-center gap-1 bg-gray-50 px-5 h-[52px] rounded-2xl cursor-pointer', className)}>
+      <ArrowUpDown size={16} />
+      <b>Сортировка:</b>
+
+      <b className="text-primary">популярное</b>
     </div>
   );
 }
