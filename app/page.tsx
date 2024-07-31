b365350574b5551f34c3b6df5b52408f58d8b23d
Sergey <m1piks1306@mail.ru>
2024-07-31 19:06:09 +0300

    add modified filters (need to finish)

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import {
   Container,
   Title,
   TopBar,
+  Filters,
 } from "@/components/shared";
 
 export default function Home() {
@@ -10,8 +11,26 @@ export default function Home() {
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
-      
+
       <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters/>
+          </div>
+
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              Список товаров
+            </div>
+          </div>
+
+        </div>
+      </Container>
     </>
   );
 }
